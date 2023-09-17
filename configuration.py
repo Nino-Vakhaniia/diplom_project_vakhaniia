e11f6ec7798adc65f25b807df338b386aa929442
Your Name <you@example.com>
2023-09-17 12:30:35 +0300

    Diplom project

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://910d06a9-d64c-49b8-81b7-13351f446e41.serverhub.praktikum-services.ru"
+CREATE_ORDER_PATH = "/api/v1/orders"
+GET_ORDER_INFO = "/api/v1/orders/track"
---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+    "firstName": "Нино",
+    "lastName": "Вахания",
+    "address": "Москва, Ленинский просеект 13",
+    "metroStation": 33,
+    "phone": "+9671643040",
+    "rentTime": 3,
+    "deliveryDate": "2023-09-17",
+    "comment": "Позвоните заранее",
+    "color": [
+        "BLACK"
+    ]
+}
